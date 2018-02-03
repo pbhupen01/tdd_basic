bc9ec7ad508533fabde77089aba1f9d91b4b4926
pbhupen01 <pandeybhupen01@gmail.com>
2018-02-03 16:37:55 +0530

    Added Cat and CatTest class

---
+++ src/main/java/com/practice/tdd/dto/Cat.java
@@ -0,0 +1,4 @@
+package com.practice.tdd.dto;
+
+public class Cat {
+}
---
+++ src/test/java/com/practice/tdd/dto/CatTest.java
@@ -0,0 +1,12 @@
+package com.practice.tdd.dto;
+
+import org.junit.Test;
+
+public class CatTest {
+
+    @Test
+    public void shouldSetNameCorrectly()
+    {
+
+    }
+}
