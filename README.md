ebf68b0d2fa8c85daf5d54724401a6970e0edc2d
pbhupen01 <pandeybhupen01@gmail.com>
2018-02-03 16:50:27 +0530

    Completed example for TDD

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # tdd_basic
+
+# In TDD tests drive the development.
+
+# 1. First tests (Junit) are written for new feature which doesn't exist
+# 2. New tests are executed to verify that they fail
+# 3. Feature is implemented for all the new tests
+# 4. Tests are run again to verify that tests pass
--- src/test/java/com/practice/tdd/dto/CatTest.java
+++ src/test/java/com/practice/tdd/dto/CatTest.java
@@ -1,12 +1,28 @@
 package com.practice.tdd.dto;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class CatTest {
 
     @Test
     public void shouldSetNameCorrectly()
     {
+        String name = "Meow";
+        Cat cat = new Cat(name);
 
+        assertTrue(cat.getName().equals(name));
+    }
+
+    @Test
+    public void shouldBeAbleToChangeNameCorrectly()
+    {
+        String name = "Meow";
+        Cat cat = new Cat(name);
+
+        String newName = "Kitty";
+        cat.setName(newName);
+
+        assertTrue(cat.getName().equals(newName));
     }
 }
